364eca475f02d3f2a9e55845d624fe35977844e3
Himawan Ihza Pradana <himawanihza@gmail.com>
2022-10-09 11:21:46 +0700

    first coommit

---
+++ script.js
@@ -0,0 +1,14 @@
+let card = document.querySelector('.card');
+let app = document.querySelector('.app')
+
+
+let text1 = document.createElement('p')
+text.innerText = "Hallo!!"
+text1.innerText = "Hallo!!!!!!"
+
+// for (let i = 0; i <= 5; i++){
+//     let text = "halo"
+//     let text = document.createElement('p')
+//     app.append(text);
+//     app.append(text1);
+// }
